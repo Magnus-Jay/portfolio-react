ff561f75b81d0493652a41db4d99e2a2151e3046
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2021-01-19 11:09:38 -0800

    updated email button on contact page

--- src/components/pages/ContactPage/Contact.js
+++ src/components/pages/ContactPage/Contact.js
@@ -30,7 +30,7 @@ function Contact(props) {
               Thank you for visiting my portfolio and I hope you enjoyed what you have seen so far. Please feel free to send me an email using the one listed below. Additionally there is a link to my LinkedIn page where you can find additional information about myself and experience. Send me a connection request and say hi!
             </p>
             {/* <br /> */}
-            <p>Email:   <a href="mailto:magnus86@uw.edu">magnus86@uw.edu</a></p>
+            <p>Email:  <button className="btn btn-dark" id="linked"><a href="mailto:magnus86@uw.edu">magnus86@uw.edu</a></button></p>
             <p>Check out my work history and my professional profile on:</p>
             <button className="btn btn-dark" id="linked"><a href="https://www.linkedin.com/in/magnusappe/">LinkedIn Profile</a></button>
             <br />
--- src/components/pages/PortfolioPage/Portfolio.js
+++ src/components/pages/PortfolioPage/Portfolio.js
@@ -86,38 +86,6 @@ function Portfolio() {
           </div>
           </div>
       </Row>
-      {/* <Row className="row" id="rowThree">
-        <Col md="auto" className="projectOne">
-          <img src={freshest} id="pic-one" alt="comet" />
-          <h5>
-           <span>
-            Fresh Air - Hiking and Food Finder
-          <a href="https://clubkemp.github.io/freshair/" target="_blank" rel="noopener noreferrer">
-          <i class="fa fa-globe mag-icon" aria-hidden="true"></i>
-          </a >
-          <a href="https://github.com/clubkemp/freshair" target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-github mag-icon" aria-hidden="true"></i>
-          </a >
-          </span>
-          </h5 > 
-        </ Col>
-      </Row>
-      <Row className="row" id="rowFour">
-        <Col md="auto" className="projectThree">
-          <img src={Note} id="pic-three" alt="deployed app link" />
-          <h5>
-           <span>
-            Note Taking Application
-          <a href="https://calm-wildwood-27402.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-          <i class="fa fa-globe mag-icon" aria-hidden="true"></i>
-          </a >
-          <a href="https://github.com/Magnus-Jay/note-taker" target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-github mag-icon" aria-hidden="true"></i>
-          </a >
-          </span>
-          </h5 > 
-        </ Col>
-      </Row> */}
     </Container>
   );
 }
